cea0d5ee0ada34cf456ebcbefba31ccd5980280a
awni <awni.hannun@gmail.com>
2017-07-08 11:10:54 -0700

    loader tests started, specgram working

--- loader.py
+++ loader.py
@@ -12,13 +12,16 @@ from speech.utils import wave
 
 class AudioDataset(tud.Dataset):
 
+    START = "<s>"
+    END = "</s>"
+
     def __init__(self, data_json, batch_size):
 
         data = read_data_json(data_json)
         chars = set(t for d in data for t in d['text'])
+        chars.update([self.START, self.END])
         self.int_to_char = dict(enumerate(chars))
         self.char_to_int = {v : k for k, v in self.int_to_char.items()}
-
         for d in data:
             d['label'] = self.encode(d['text'])
 
@@ -31,10 +34,18 @@ class AudioDataset(tud.Dataset):
         self.idxs = [i for b in batch_idxs for i in b]
 
         self.data = data
-        self.output_dim = len(chars)
         self.batch_size = batch_size
 
+    @property
+    def input_dim(self):
+        return 161 # TODO
+
+    @property
+    def output_dim(self):
+        return len(self.char_to_int)
+
     def encode(self, text):
+        text = [self.START] + list(text) + [self.END]
         return [self.char_to_int[t] for t in text]
 
     def __len__(self):
@@ -42,8 +53,7 @@ class AudioDataset(tud.Dataset):
 
     def __getitem__(self, idx):
         datum = self.data[self.idxs[idx]]
-        inputs = specgram_from_file(datum['audio'])
-        print("IDX", idx, "SHAPE", inputs.shape)
+        inputs = log_specgram_from_file(datum['audio'])
         targets = self.encode(datum['text'])
         return inputs, targets
 
@@ -52,24 +62,35 @@ class AudioDataset(tud.Dataset):
         random.shuffle(idxs)
         samples = [self[idx][0]
                    for idx in idxs[:max_samples]]
-        samples = np.hstack(samples)
-        mean = np.mean(samples, axis=1)
-        std = np.std(samples, axis=1)
+        samples = np.vstack(samples)
+        mean = np.mean(samples, axis=0)
+        std = np.std(samples, axis=0)
         return mean, std
 
 def zero_pad_concat(inputs):
     # Assumes last item in batch is the longest.
     shape = inputs[-1].shape
-    shape = (len(inputs),shape[0], shape[1])
+    shape = (len(inputs), shape[0], shape[1])
     cat_inputs = np.zeros(shape, dtype=np.float32)
     for e, inp in enumerate(inputs):
-        cat_inputs[e, :, :inp.shape[1]] = inp
+        cat_inputs[e, :inp.shape[0], :] = inp
     return cat_inputs
+
+def end_pad_concat(labels):
+    # Assumes last item in each example is the end token.
+    batch_size = len(labels)
+    end_tok = labels[0][-1]
+    max_len = max(len(l) for l in labels)
+    cat_labels = np.full((batch_size, max_len),
+                    fill_value=end_tok, dtype=np.int64)
+    for e, l in enumerate(labels):
+        cat_labels[e, :len(l)] = l
+    return cat_labels
 
 def collate_fn(batch):
     inputs, labels = zip(*batch)
     inputs = zero_pad_concat(inputs)
-    # TODO convert inputs and labels to tensors.
+    labels = end_pad_concat(labels)
     return inputs, labels
 
 def make_loader(dataset, num_workers=0):
@@ -82,12 +103,11 @@ def make_loader(dataset, num_workers=0):
                 drop_last=True)
     return loader
 
-def specgram_from_file(audio_file):
+def log_specgram_from_file(audio_file):
     audio, sr = wave.array_from_wave(audio_file)
-    return specgram(audio, sr)
+    return log_specgram(audio, sr)
 
-def specgram(audio, sample_rate, window_size=20, step_size=10):
-    ## TODO, compute LOG!! And viz some of these.
+def log_specgram(audio, sample_rate, window_size=20, step_size=10):
     nperseg = window_size * sample_rate / 1e3
     noverlap = step_size * sample_rate / 1e3
     _, _, spec = scipy.signal.spectrogram(audio,
@@ -96,11 +116,8 @@ def specgram(audio, sample_rate, window_size=20, step_size=10):
                     nperseg=nperseg,
                     noverlap=noverlap,
                     detrend=False)
-    return spec.astype(np.float32)
+    return np.log(spec.T.astype(np.float32))
 
 def read_data_json(data_json):
     with open(data_json) as fid:
         return [json.loads(l) for l in fid]
-
-if __name__ == "__main__":
-    dataset = AudioDataset("test.json")
--- test/loader_test.py
+++ test/loader_test.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 from speech import loader
 
 def test_dataset():
     batch_size = 2
-    data_json = "test/test.json"
-    dataset = loader.AudioDataset("test.json", batch_size)
+    data_json = "test.json"
+    dataset = loader.AudioDataset(data_json, batch_size)
 
     # Correct number of examples
     assert len(dataset.data) == 8
@@ -11,3 +13,8 @@ def test_dataset():
     # Num chars plus start and end tokens
     assert dataset.output_dim == 11
 
+    inputs, targets = dataset[0]
+    # Inputs should be time x frequency
+    assert inputs.shape[1] == dataset.input_dim
+
+    assert inputs.dtype == np.float32
