068d9c4b41e11a599341a0a7a499a55fb1ea6410
awni <awni.hannun@gmail.com>
2017-07-20 14:24:54 -0700

    compare decode with decode step, should merge these / refactor if possible

--- test/model_test.py
+++ test/model_test.py
@@ -12,11 +12,33 @@ def test_model():
     batch_size = 4
     seq_len = 20
 
+    config = {
+        "encoder_layers" : 1,
+        "rnn_dim" : 16
+    }
 
-    model = speech.model.Model(freq_dim, output_dim)
+    np.random.seed(1337)
+    torch.manual_seed(1337)
+
+    model = speech.model.Model(freq_dim, output_dim, config)
 
     x = autograd.Variable(torch.randn(batch_size, time_steps, freq_dim))
     labels = np.random.randint(0, output_dim, (batch_size, seq_len))
     y = autograd.Variable(torch.LongTensor(labels))
     output = model.forward(x, y)
+
+    x_enc = model.encode(x)
+    out, _, _ = model.decode(x_enc, y)
+
+    hx = None
+    out_s = []
+    for t in range(seq_len - 1):
+        ox, hx = model.decode_step(x_enc, y[:,t], hx=hx)
+        out_s.append(ox)
+    out_s = torch.stack(out_s, dim=1)
+    assert out.size() == out_s.size()
+    assert np.allclose(out_s.data.numpy(),
+                       out.data.numpy(),
+                       rtol=1e-5,
+                       atol=1e-7)
 
